b5fa82457ede7b3509b045ab31cee9bc9459dd22
mima ch <mima.cherfa19@hotmail.fr>
2022-04-05 13:17:57 +0200

    dodif js

--- modal.js
+++ modal.js
@@ -7,10 +7,18 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// DOM Elements launch
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
+    //DOM elements (Variables)
+    const prenom = document.getElementById('first');
+    const nom = document.getElementById('last');
+    const email = document.getElementById('email');
+    const birthdate = document.getElementById('birthdate');
+    const quantity = document.getElementById('quantity');
+    const citySelector = document.getElementsByName("location");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +27,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
 
 
